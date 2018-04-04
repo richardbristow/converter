83f9efe867d24748d615f16b5c783835bcf893ca
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2018-04-04 13:38:10 +0100

    Changed proptype to make the ooption object be very specific in shape

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -2,17 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Dropdown = ({
-  name, menuItems, value, handleChange,
+  name, options, value, handleChange,
 }) => (
   <select name={name} value={value} onChange={handleChange}>
-    {menuItems.map(({ mathName, displayName }) =>
+    {options.map(({ mathName, displayName }) =>
       <option key={`${name}-${mathName}`} value={`${mathName}`}>{`${displayName}`}</option>)}
   </select>
 );
 
 Dropdown.propTypes = {
   name: PropTypes.string.isRequired,
-  menuItems: PropTypes.arrayOf(PropTypes.object).isRequired,
+  options: PropTypes.arrayOf(PropTypes.shape({
+    displayName: PropTypes.string.isRequired,
+    mathName: PropTypes.string.isRequired,
+  })).isRequired,
   value: PropTypes.string.isRequired,
   handleChange: PropTypes.func.isRequired,
 };
