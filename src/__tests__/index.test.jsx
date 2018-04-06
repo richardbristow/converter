5a82730fd728feaab7b6da406325e2bbe8b1f2cb
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2018-04-06 16:17:19 +0100

    Initial commit of index tests

--- src/__tests__/index.test.jsx
+++ src/__tests__/index.test.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 import Converter from '../components/Converter';
+// import Index from '../index';
 
 describe('index', () => {
-  it('renders app without errors', () => {
+  it('should render converter app without errors', () => {
     const wrapper = shallow(<Converter />);
+    const inst = wrapper.instance();
+    expect(inst).toBeInstanceOf(Converter);
     expect(wrapper).toHaveLength(1);
+  });
+
+  it('matches the snapshot', () => {
+    const tree = renderer.create(<Converter />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
