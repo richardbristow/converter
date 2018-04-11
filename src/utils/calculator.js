48521e37566caa44b3c96de9dddd29c1a7ae3fd2
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2018-04-11 21:39:08 +0100

    Initial commit of the conversion calculator functions

---
+++ src/utils/calculator.js
@@ -0,0 +1,20 @@
+import math from 'mathjs';
+
+export const leftToRight = (input, leftUnit, rightUnit) => {
+  const inputUnit = math.unit(input, leftUnit);
+  return inputUnit.to(rightUnit);
+};
+
+export const rightToLeft = (input, leftUnit, rightUnit) => {
+  const inputUnit = math.unit(input, rightUnit);
+  return inputUnit.to(leftUnit);
+};
+
+export const tryConvert = (val, leftUnit, rightUnit, convert) => {
+  const input = parseFloat(val);
+  if (Number.isNaN(input)) {
+    return '';
+  }
+  const output = convert(input, leftUnit, rightUnit);
+  return output.toString();
+};
