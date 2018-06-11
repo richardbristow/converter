72a880ef55cf521e10e1ac1f7c6518c6116495aa
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2018-06-11 15:26:58 +0100

    Handle 404 routes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,6 +5,8 @@ import { Route, Switch } from 'react-router-dom';
 import Help from './navbar/Help';
 import About from './navbar/About';
 import InputGroup from './converter-inputs/InputGroup';
+import NoRoute from './NoRoute';
+import conversions from '../constants/conversions';
 
 const Main = ({ convert }) => (
   <Switch>
@@ -17,12 +19,17 @@ const Main = ({ convert }) => (
     />
     <Route path="/help" component={Help} />
     <Route path="/about" component={About} />
-    <Route
-      path="/:conversionType"
-      render={({ match }) => (
-        <InputGroup convert={convert} conversionType={match.params.conversionType} />
-      )}
-    />
+    {Object.keys(conversions).map((type) => {
+      const { mathName } = conversions[type];
+      return (<Route
+        key={`route-${mathName}`}
+        path={`/${mathName}`}
+        render={({ match }) => (
+          <InputGroup convert={convert} conversionType={match.params.conversionType} />
+        )}
+      />);
+    })}
+    <Route component={NoRoute} />
   </Switch>
 );
 
---
+++ src/components/NoRoute.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NoRoute = () => (
+  <div>
+    Uh-oh! No page exists why dont you go back home.
+  </div>
+);
+
+export default NoRoute;
