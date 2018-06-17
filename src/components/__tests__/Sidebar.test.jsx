1273d9ce285925d792a5024c36e1be2d2ba931ec
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2018-06-17 12:25:55 +0100

    Fixed sidebar test

--- src/components/__tests__/Sidebar.test.jsx
+++ src/components/__tests__/Sidebar.test.jsx
@@ -5,9 +5,8 @@ import Sidebar from '../Sidebar';
 import conversions from '../../constants/conversions';
 
 describe('Sidebar', () => {
-  // it('should have the same number of links as there are conversion types in conversions.js', () => {
-  //   const wrapper = shallow(<Sidebar items={conversions} />);
-  //   wrapper.html();
-    
-  // });
+  it('should have the same number of links as there are conversion types in conversions.js', () => {
+    const wrapper = shallow(<Sidebar items={conversions} />);
+    expect(wrapper.find('li')).toHaveLength(Object.keys(conversions).length);  
+  });
 });
