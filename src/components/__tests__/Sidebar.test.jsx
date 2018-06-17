dc416dfbdc612f319fb66d4e5c953309b42c6ea3
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2018-06-17 12:27:26 +0100

    removed trailing whitespace

--- src/components/__tests__/Sidebar.test.jsx
+++ src/components/__tests__/Sidebar.test.jsx
@@ -7,6 +7,6 @@ import conversions from '../../constants/conversions';
 describe('Sidebar', () => {
   it('should have the same number of links as there are conversion types in conversions.js', () => {
     const wrapper = shallow(<Sidebar items={conversions} />);
-    expect(wrapper.find('li')).toHaveLength(Object.keys(conversions).length);  
+    expect(wrapper.find('li')).toHaveLength(Object.keys(conversions).length);
   });
 });
