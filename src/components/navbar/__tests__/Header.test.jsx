8d6a781c31f74eb107094aaee5b89b0ce2bcda89
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2018-06-17 12:26:27 +0100

    Added Header component test

---
+++ src/components/navbar/__tests__/Header.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Header from '../Header';
+
+describe('Header', () => {
+  it('should render the header', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should show two links', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find('Link')).toHaveLength(2);
+  });
+});
