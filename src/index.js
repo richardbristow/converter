960b8a5187732874a523eb8777c0a6363c247119
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2018-07-03 20:51:29 +0100

    Injected normalize.css into app

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styledNormalize from 'styled-normalize';
+import { injectGlobal } from 'styled-components';
 
 import Converter from './components/Converter';
 import registerServiceWorker from './registerServiceWorker';
+
+injectGlobal`
+  ${styledNormalize}
+`;
 
 ReactDOM.render(<Converter />, document.getElementById('root'));
 registerServiceWorker();
