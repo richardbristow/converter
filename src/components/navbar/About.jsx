ac85dd90ee2f42d2d4d034291a369c4090008d60
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2018-07-17 18:15:23 +0100

    Updated about page placeholder

--- src/components/navbar/About.jsx
+++ src/components/navbar/About.jsx
@@ -2,7 +2,19 @@ import React from 'react';
 
 const About = () => (
   <div>
-    This is the about page.
+    <h1>About</h1>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+       Delectus deleniti nam beatae sint voluptas ea blanditiis,
+       sequi itaque animi vero nisi aperiam necessitatibus
+       mollitia est voluptates nemo sit at temporibus?
+    </p>
+    <h2>Credits</h2>
+    <ul>
+      <li>react</li>
+      <li>math.js</li>
+      <li>fixe</li>
+    </ul>
   </div>
 );
 
