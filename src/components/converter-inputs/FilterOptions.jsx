987b526679b2db412495d0f37e4da68e7f9d848d
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2018-07-25 22:15:46 +0100

    Initial commit of keypressdown functionality

--- src/components/converter-inputs/FilterOptions.jsx
+++ src/components/converter-inputs/FilterOptions.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 
+const focusNextOption = (name, value, nextOption, refs) => {
+  if (refs[nextOption]) {
+    refs[nextOption].current.focus();
+  }
+};
+
+const onArrowKeyDown = (options, refs, e) => {
+  const { key, target } = e;
+  const { name, value } = target;
+  const optionsMathNames = options.map(option => option.mathName);
+  if (key === 'ArrowDown') {
+    const nextOption = optionsMathNames.indexOf(value) + 1;
+    focusNextOption(name, value, nextOption, refs);
+  } else if (key === 'ArrowUp') {
+    const nextOption = optionsMathNames.indexOf(value) - 1;
+    focusNextOption(name, value, nextOption, refs);
+  }
+};
+
 const FilterOptions = ({
   options, name, conversionType, filter, handleDropdownItemClick,
 }) => {
   const filteredOptions = options.filter(option => (
     option.displayName.toLowerCase().indexOf(filter.toLowerCase()) !== -1
   ));
+  const dropdownOptionRefs = filteredOptions.map(() => React.createRef());
   return (
     <div>
       {filteredOptions.length > 0 ? (filteredOptions.map(({ mathName, displayName }, index) => (
@@ -15,6 +35,8 @@ const FilterOptions = ({
           key={`${name}-${mathName}`}
           value={mathName}
           onClick={e => handleDropdownItemClick(conversionType, e)}
+          onKeyDown={e => onArrowKeyDown(filteredOptions, dropdownOptionRefs, e)}
+          ref={dropdownOptionRefs[index]}
         >
           {displayName}
         </button>))) : <p>No units found.</p>}
