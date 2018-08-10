db6ad3857bbfa1274cffe3f1206f757c8a088f88
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2018-08-10 20:41:50 +0100

    Convert arrows are now icons

--- src/components/converter-inputs/ConvertArrows.jsx
+++ src/components/converter-inputs/ConvertArrows.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import icons from '../../constants/icons/icons';
+
+import Icon from '../shared/Icon';
+
 const StyledConvertArrows = styled.div`
   grid-area: arrows;
+  display: grid;
+  grid-template-columns: 1fr;
+  align-items: center;
+  padding: 0px 15px;
+`;
+
+const StyledArrow = styled.div`
+  ${props => (props.flip && 'transform: rotate(180deg);')}
 `;
 
 const ConvertArrows = () => (
   <StyledConvertArrows>
-    <div>⇀</div>
-    <div>↽</div>
+    <StyledArrow>
+      <Icon size="60" icon={icons.arrow} />
+    </StyledArrow>
+    <StyledArrow flip>
+      <Icon size="60" icon={icons.arrow} />
+    </StyledArrow>
   </StyledConvertArrows>
 );
 
