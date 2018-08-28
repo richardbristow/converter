f49dd3ed6d19e665c073538d4e635e2d3719fbd5
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2018-08-28 14:47:01 +0100

    Added link to go home on 404 error

--- src/components/NoRoute.jsx
+++ src/components/NoRoute.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NoRoute = () => (
   <div>
-    Uh-oh! No page exists why dont you go back home.
+    Uh-oh! No page exists why dont you go back <Link to="/length">home</Link>.
   </div>
 );
 
