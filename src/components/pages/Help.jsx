8e81112c4d2a4314761bf31c6a50801d1e41bc7c
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2018-08-28 14:47:01 +0100

    Added link to go home on 404 error

--- src/components/pages/Help.jsx
+++ src/components/pages/Help.jsx
@@ -5,7 +5,12 @@ import StyledNavPage from '../shared/StyledNavPage';
 const Help = () => (
   <StyledNavPage>
     <h2>Help</h2>
-    This is the help page.
+    <p>This is the help page.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Modi autem quo voluptatum corrupti vitae, et blanditiis
+      tempora inventore nostrum quam neque eum magnam velit
+      pariatur voluptates architecto repellat saepe recusandae!
+    </p>
   </StyledNavPage>
 );
 
