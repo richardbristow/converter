a2a099d371f026f158863ec4fa160a2191e2a91a
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2018-09-23 18:42:07 +0100

    Initial commit of update updateExchangeRatesCache

---
+++ backend/lambda/updateExchangeRatesCache.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-console */
+
+const fetch = require('node-fetch');
+
+const exchangeRatesTablePut = require('../utils/exchangeRatesTablePut');
+
+exports.updateExchangeRatesCache = async () => {
+  try {
+    const url = 'http://data.fixer.io/api/latest?access_key=';
+    const apiResponse = await fetch(`${url}${process.env.FIXER_API_KEY}`);
+    const ratesData = await apiResponse.json();
+    console.log('Success fetching ratesData');
+
+    const params = {
+      TableName: 'ExchangeRatesTable',
+      Item: {
+        id: 'exchangeRates',
+        dynamoCreatedAt: Date.now(),
+        ...ratesData,
+      },
+      ReturnValues: 'ALL_OLD',
+    };
+
+    const dynamoResponse = await exchangeRatesTablePut(params);
+    console.log('replaced old item', dynamoResponse);
+    console.log('put new item', params.Item);
+    return params.Item;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
---
+++ backend/utils/exchangeRatesTablePut.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-console */
+
+const AWS = require('aws-sdk');
+
+AWS.config.update({ region: 'us-east-1' });
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+const exchangeRatesTablePut = params => (
+  new Promise((resolve, reject) => {
+    dynamoDb.put(params, (error, data) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve(data);
+    });
+  })
+);
+
+module.exports = exchangeRatesTablePut;
