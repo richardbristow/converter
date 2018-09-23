3252a7e72d746bd6e87478ed48267db37b552907
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2018-09-23 19:15:36 +0100

    Initial commit of updateCurrencySymbolsCache

---
+++ backend/lambda/updateCurrencySymbolsCache.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-console */
+
+const fetch = require('node-fetch');
+
+const exchangeRatesTablePut = require('../utils/exchangeRatesTablePut');
+
+exports.updateCurrencySymbolsCache = async () => {
+  try {
+    const url = 'http://data.fixer.io/api/symbols?access_key=';
+    const apiResponse = await fetch(`${url}${process.env.FIXER_API_KEY}`);
+    const symbolsData = await apiResponse.json();
+    console.log('Success fetching symbolsData');
+
+    const params = {
+      TableName: 'ExchangeRatesTable',
+      Item: {
+        id: 'currencySymbols',
+        dynamoCreatedAt: Date.now(),
+        ...symbolsData,
+      },
+      ReturnValues: 'ALL_OLD',
+    };
+
+    const dynamoResponse = await exchangeRatesTablePut(params);
+    console.log('replaced old item', dynamoResponse);
+    console.log('put new item', params.Item);
+    return params.Item;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
