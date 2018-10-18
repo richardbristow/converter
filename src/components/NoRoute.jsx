adf80d8a3d0ab0f788a28bb15191ab154f9ab292
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2018-10-18 13:07:14 +0100

    Converted div to Fragment

--- src/components/NoRoute.jsx
+++ src/components/NoRoute.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NoRoute = () => (
-  <div>
+  <React.Fragment>
     Uh-oh! No page exists why dont you go back&nbsp;
     <Link to="/length">home</Link>
     .
-  </div>
+  </React.Fragment>
 );
 
 export default NoRoute;
