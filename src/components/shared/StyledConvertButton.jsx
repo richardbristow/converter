5b8a1f1e2961971d19001545cad8b37d0ee325ee
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2018-10-18 14:10:49 +0100

    Reverted eslint to 5.6.0
    V5.7.0 not supported currently by create-react-app

--- src/components/shared/StyledConvertButton.jsx
+++ src/components/shared/StyledConvertButton.jsx
@@ -6,7 +6,7 @@ const StyledConvertButton = styled.button`
   background-color: ${({ selected, theme }) => (selected ? `${theme.selectedBackground}` : 'whitesmoke')};;
   border-top: 0px;
   border-bottom: ${({ header }) => (header ? '1px solid black' : '0px')};
-  border-left: ${({ header }) => header && '1px solid black'};
+  border-left: ${({ header }) => (header ? '1px solid black' : '0px')};
   border-right: 0px;
   border-radius: ${({ header }) => header && '0px 0px 0px 4px'};
   outline: none;
