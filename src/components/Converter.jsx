c512a3392fa936bebe336e0b0e8248b65000d2e9
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2019-02-17 12:59:05 +0000

    Added netlify.toml for manual deploy from github actions

--- src/components/Converter.jsx
+++ src/components/Converter.jsx
@@ -109,7 +109,7 @@ class Converter extends Component {
       <ThemeProvider theme={globalTheme}>
         <React.Fragment>
           <GlobalStyle />
-          <Router basename={process.env.PUBLIC_URL}>
+          <Router>
             <StyledConverter
               onBlur={this.onBlurHandler}
               onFocus={this.onFocusHandler}
