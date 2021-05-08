d6fbde9de5d40597c812e56024bca1cb057f0b72
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2021-05-08 19:55:50 +0100

    added router to index tests

--- src/__tests__/index.test.jsx
+++ src/__tests__/index.test.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
+import { BrowserRouter as Router } from 'react-router-dom';
 import '../__mocks__/matchMedia';
 
 import Converter from '../components/Converter';
 
 describe('index', () => {
   it('should render converter app without errors', () => {
-    const wrapper = shallow(<Converter />);
+    const wrapper = shallow(
+      <Router>
+        <Converter />
+      </Router>
+    );
     // const inst = wrapper.instance();
     // expect(inst).toBeInstanceOf(Converter);
     expect(wrapper).toHaveLength(1);
   });
 
   it('matches the snapshot', () => {
-    const tree = renderer.create(<Converter />).toJSON();
+    const tree = renderer
+      .create(
+        <Router>
+          <Converter />
+        </Router>
+      )
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
